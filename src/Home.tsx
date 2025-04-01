03bc4f4424a4a4511f2bb49f44d6ead33c2eea9c
Hyo <lovellehyo@gmail.com>
2025-04-01 14:48:10 +0900

    Tailwind CSS 초기화

---
+++ src/Home.tsx
@@ -0,0 +1,10 @@
+// src/pages/Home.tsx
+const Home = () => {
+    return (
+        <main className="flex flex-col items-center justify-center min-h-[calc(100vh-128px)] py-12">
+
+        </main>
+    );
+};
+
+export default Home;
